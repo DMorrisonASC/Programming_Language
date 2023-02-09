d1975c66321b8687e35f11f55da1198cd1d36f12
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2023-02-08 21:01:58 -0500

    Update Tester.cpp

--- HW_2/Tester.cpp
+++ HW_2/Tester.cpp
@@ -1,10 +1,33 @@
 #include "Point.h"
 #include<cstdio>
 
+void changeX_cord (Point& item, int num);
+
 int main() {
 	Point dot(1,2);
 	Point circle(4,8);
+	Point * ref_circle; 
+	ref_circle = &circle;
+	
 	// printf(dot.dis(dot, circle));
 	printf("%f", dot.dis(dot, circle));
+	
+	// changeX_cord(ref_circle, 1);
+	printf("\n%d", circle.getX());
+	
+	// printf("\n%f", ref_circle);
+	// Test if "ref_circle" correctly recieved the address of "circle"
+	// printf("\n%p", ref_circle);
+	// printf("\n%p", &circle);
+	
+}
+
+void changeX_cord (Point& ref, int num) {
+	// item = new Point(4,8);
+	// int new_cordX = item.getX() + num;
+	// item.setX(new_cordX);
+	Point item = ref;
+	int new_cordX = item.getX() + num;
+	item.setX(new_cordX);
 	
 }
