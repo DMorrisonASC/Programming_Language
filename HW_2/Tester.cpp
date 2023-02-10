191fd0e65103de305d00bb6b97e817b32a4d129f
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2023-02-09 20:21:09 -0500

    Update Tester.cpp

--- HW_2/Tester.cpp
+++ HW_2/Tester.cpp
@@ -1,50 +1,67 @@
 #include "Point.h"
 #include<cstdio>
+#include <iostream>
 
 void changeX_cord (Point* item, int num);
-// Allocate 2 Point objects on the heap (don't forget to delete them)
-Point* tempTip1(int x, int y);
-Point* tempTip2(int x, int y);
-
+void changeX_cord (Point item, int num);
 
 int main() {
+	// Allocate 2 Point objects on the heap (don't forget to delete them)
+	Point * tempPointer;
+	Point * tempPointer2;
+	//
 	Point dot(1,2);
 	Point circle(4,8);
 	Point * ref_circle; 
 	ref_circle = &circle;
-	// Allocate 2 Point objects on the heap (don't forget to delete them)
-	// Point* tip1 = tempTip(7,3);
 	
 	// print distance between "dot" and "circle" Point objects.
 	printf("%f", dot.dis(dot, circle));
 	
-	changeX_cord( &circle, 19);
-	printf("\n%d", circle.getX());
+	changeX_cord(ref_circle, 19);
+	changeX_cord(circle, 19);
+	printf("\n%d\n", circle.getX());
+	
 	// Test if "ref_circle" correctly recieved the address of "circle"
 	// printf("\n%p", ref_circle);
 	// printf("\n%p", &circle);
 	
+	delete tempPointer;
+	delete tempPointer2;
+	
+	// 4) Make an array of Points and use pointers to access them. 
+	// Make sure that you call at least 
+	// 2 methods in a loop that access all elements of the array.
+	int numOfPoints = 5;
+	Point p[numOfPoints];
+	
+	for (int i = 0; i < numOfPoints; i++ ) {
+		p[i].setX(i);
+		p[i].setY(i);
+	}
+	
+	for (int i = 0; i < numOfPoints; i++) {
+		// print distance between each point
+		// printf("\n%f", dot.dis(dot, circle));
+		if ( i < numOfPoints - 1) {
+		std::cout << "\nThis distance between " << "("
+		<< p[i].getX() << ", " << p[i].getY() << ")" 
+		<< " & " 
+		<< "(" << p[i+1].getX() << ", " << p[i+1].getY() << ")" 
+		<< " is " 
+		<< p[i].dis(p[i], p[i+1]) << std::endl;
+		}
+	}
 }
-
+// pass by reference
 void changeX_cord (Point* ref, int num) {
-	// item = new Point(4,8);
-	// int new_cordX = item.getX() + num;
-	// item.setX(new_cordX);
-	// Point item = ref;
-	Point *item = ref;
+	Point * item = ref;
 	int new_cordX = item->getX() + num;
 	item->setX(new_cordX);
-	// printf("\n%d", item.getX());
-	// printf("\n%d", new_cordX);
 }
-
-// Point* tempTip(int x, int y){
-	// Point* tip1;
-	// pTwo = new Point(x,y);
-	// std::cout << tip1;
-	// std::cout<<"\n\n";
-	// std::cout<< tip1->getX();
-	// std::cout<<"\n\n";
-	// return tip1;
-	// delete tip1;
-// }
+// pass by value
+void changeX_cord (Point ref, int num) {
+	
+	int new_cordX = ref.getX() + num;
+	ref.setX(new_cordX);
+}
