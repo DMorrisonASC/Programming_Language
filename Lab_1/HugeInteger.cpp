f5d5903a66f6ed780db55edfd0dd498520abacad
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2023-02-17 13:31:39 -0500

    Update HugeInteger.cpp

--- Lab_1/HugeInteger.cpp
+++ Lab_1/HugeInteger.cpp
@@ -1,6 +1,8 @@
 #include "HugeInteger.h"
 #include<vector>
 #include<string>
+#include <iostream>
+#include <sstream>
 using namespace std;
 
 	HugeInteger::HugeInteger(){
@@ -12,36 +14,86 @@ using namespace std;
 	HugeInteger::HugeInteger(const HugeInteger & other) {
 		// receives memeory location of object type `HugeInteger`, called other.
 		// instancizaes your own copy;
-		data = new vector<int>();
+		this->data = new vector<int>();
 		// Copy values of the vector `other` to this.data to make a "deep copy"
+		// for (int number : other.data) {
+				// data->push_back(number);
+			// }
+		this->data = new vector<int>();
+		for (auto i = other.data->begin(); i != other.data->end(); ++i)
+		{
+			data->push_back(*i);
+		}
 		
 	}
 	
 	HugeInteger::HugeInteger( const string & stringita ) {
 		data = new vector<int>();
 		// traverse through the string
-		for (int i = 0; i < stringita.size(); i++) {
-			data->push_back(i);
+		for (char letter : stringita) {
+			int asciiNum = int(letter);
+			if ( 48 <= asciiNum <= 57) {
+				data->push_back(letter);
+			}
 		}
 	}
 	
 	bool HugeInteger::operator == (const HugeInteger & rightSide) {
 		
+		// if (this->data->size() != rightSide.data->size()) {
+			// return false;
+		// }
+		
+		for (int i = 0; i < this->data->size(); i++)
+		{
+			if (this->data[i] != rightSide.data[i]) {
+				return false;
+			}
+		}
+		return true;
 	}
+	
 	const operator + (const HugeInteger & rightSide) {
+		int addedNum = 0;
+		
+		// this.data +
 		
 	}
+	
 	bool HugeInteger::operator > (const HugeInteger & rightSide) {
+		if (this->data->size() > rightSide.data->size()) {
+			return true;
+		}
+		else {
+			return false;
+		}
 
 	}
 	bool HugeInteger::isZero() const {
-		
+		// Note: the auto keyword is used to automatically identify the type of elements in range-expression
+		for (auto i = data->begin(); i < data->end(); i++) {
+			if (*i != 0)
+				return false;
+		}
+		return true;
 	}
 	string HugeInteger::toString() const {
+		// string finalString = "";
 		
+		// for(int i = 0; i < this->data->size(); i++)
+		// {
+			// string numberString = to_string(data[i]);
+			// finalString.append(numberString);
+		// }
+		// return finalString;
+		std::stringstream numberString;
+		for(auto i = data->begin(); i < data->end(); i++) {
+		  numberString << *i;
+		}
+		std::string finalString = numberString.str();
 	}
 	
 	// destructor for methods that use heaps and pointers;
 	HugeInteger::~HugeInteger() {
-		delete data;
+		delete this->data;
 	}
