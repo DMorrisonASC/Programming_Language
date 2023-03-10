a23ca524b07b6b0949a562a4a29cbe41e9612df0
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2023-03-10 14:38:53 -0500

    Create Player.h

---
+++ Exam_1/Player.h
@@ -0,0 +1,21 @@
+/*
+    Author: Daeshaun Morrison, Muhlenberg College class of 2024(daeshaunkmorrison@gmail.com)
+    Date: 2/13/2023
+    Instructor: Professor Silveyra
+    Description: 
+    Errors: 
+ */
+
+#include<string>
+using namespace std;
+
+class Player {
+	private:
+		int score;
+		int loses;
+	public:
+		Player();
+		int getScore();
+		int getLoses();
+
+};
