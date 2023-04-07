36ff3cfb75eeb9778faf8e0780615be72260cdaa
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2023-04-07 00:25:08 -0400

    Update Part_2.py

--- Lab_4/Part_2.py
+++ Lab_4/Part_2.py
@@ -10,9 +10,9 @@ def main():
             name_dict[eachKeyDict] = eachList
 
     # print(name_dict)
-    startSearching()
+    startSearching(name_dict)
 
-def startSearching():
+def startSearching(allNames_Dict):
     while True:
         # Get user input
         choice = input("Enter a choice: 1) Search 2) Popularity over time 3) Top ten: 4) Quit\n")
@@ -20,7 +20,7 @@ def startSearching():
         # Check if the user entered "a"
         if choice == "1":
             substring = input("Enter a enter name to search for: \n")
-            matching_names = searchNames(substring)
+            matching_names = searchNames(substring, allNames_Dict)
 
             if matching_names:
                 print("Names containing '{}' as a substring:".format(substring))
@@ -47,32 +47,49 @@ def startSearching():
         else:
             print("Invalid choice. Please try again.")
 
-def searchNames(substring):
+def searchNames(substring, name_dict):
     matching_names = []
-    for line in name_dict:
-        if substring in name:
-            matching_names.append(name)
+    for key in name_dict:
+        eachList = name_dict[key]
+        # print(eachName)
+        if substring.lower() in eachList[0].lower():
+            name_and_year = findBiggestYear(eachList)
+            matching_names.append(name_and_year)
     return matching_names
 
-def findYear(index):
-    indexInt = int(index)
+def findBiggestYear(nameList):
+    biggestNum_index = 1  # Skip first value(the name)
+    # Find index of biggest number
+    for i in range(2, len(nameList)):
+        if nameList[i] > nameList[biggestNum_index]:
+            biggestNum_index = i
     year = 0
-    if (indexInt == 1):
+    if (biggestNum_index == 1):
         year = "1900"
-    elif (indexInt == 2):
+    elif (biggestNum_index == 2):
         year = "1910"
-    elif (indexInt == 3):
-        year = "1910"
-    elif (indexInt == 4):
-        year = "1910"
-    elif (indexInt == 5):
-        year = "1910"
-    elif (indexInt == 6):
-        year = "1910"
-    elif (indexInt == 2):
-        year = "1910"
-    elif (indexInt == 2):
-        year = "1910"
+    elif (biggestNum_index == 3):
+        year = "1920"
+    elif (biggestNum_index == 4):
+        year = "1930"
+    elif (biggestNum_index == 5):
+        year = "1940"
+    elif (biggestNum_index == 6):
+        year = "1950"
+    elif (biggestNum_index == 7):
+        year = "1960"
+    elif (biggestNum_index == 8):
+        year = "1970"
+    elif (biggestNum_index == 9):
+        year = "1980"
+    elif (biggestNum_index == 10):
+        year = "1990"
+    elif (biggestNum_index == 11):
+        year = "2000"
+    elif (biggestNum_index == 12):
+        year = "2010"
+    
+    return nameList[0], year
 
 
 if __name__ == '__main__':
