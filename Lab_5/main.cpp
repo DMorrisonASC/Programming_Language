0c6d101a10b7ec43cda5ef9f623b0d946a8020b2
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2023-04-18 20:35:15 -0400

    Add files via upload

---
+++ Lab_5/main.cpp
@@ -0,0 +1,14 @@
+/*
+    Author: Daeshaun Morrison, Muhlenberg College class of 2024(daeshaunkmorrison@gmail.com)
+    Date: 3/21/2023
+    Instructor: Professor Silveyra
+    Description: 
+    Errors:
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, world!\n";
+    return 0;
+}
