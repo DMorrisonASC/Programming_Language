57977db1a2359c5437ce4bc6a09a5b18cc3c4f5e
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2023-04-18 20:27:34 -0400

    Create Grammar.cpp

---
+++ Lab_5/Grammar.cpp
@@ -0,0 +1,31 @@
+/*
+    Author: Daeshaun Morrison, Muhlenberg College class of 2024(daeshaunkmorrison@gmail.com)
+    Date: 3/21/2023
+    Instructor: Professor Silveyra
+    Description: 
+    Errors:
+ */
+#include "Grammar.h"
+using std::ifstream;
+using std::string;
+using namespace std;
+
+	Grammar::Grammar(){
+		int a = 1;
+	}
+
+	void Grammar::addProduction(string nonTerm, string rhs) {
+		
+	}
+
+	string Grammar::getRandomRHS(string nonTerm) {
+		return nonTerm;
+	}
+
+	bool containsNonTerminal(string nonTerm) {
+		return true;
+	}
+
+/* 	void Grammar::operator<<(Grammar sentence) {
+		
+	} */
