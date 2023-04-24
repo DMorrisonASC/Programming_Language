2c3a08414b98de0833e06fa821a31f54da8ecc90
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2023-04-24 14:06:38 -0400

    Update RandomSentenceGenerator.cpp

--- Lab_5/RandomSentenceGenerator.cpp
+++ Lab_5/RandomSentenceGenerator.cpp
@@ -20,18 +20,48 @@ using namespace std;
 	}	
 	RandomSentenceGenerator::RandomSentenceGenerator(const string & fileName) {
 		
+		this->grammarRule = new Grammar();
+		
 		ifstream file(fileName);
 		if (!file.is_open()) {
 			cerr << "File not available" << fileName << std::endl;
 		}
-
+		
+		string hashKey = "";
+		string vecData = "";
 		string line;
+		
+		bool startProduction = false;
+        bool endProduction = true;
+		
 		while (getline(file, line)) {
 			// Parse line here
-			// cout << line << endl;
+			cout << line << endl;
+			if (line.find("{") != std::string::npos) {
+				startProduction = true;
+                endProduction = false;
+                getline(file, line);
+                hashKey = line.c_str();
+				cout << hashKey << "\n";
+			}
+			
+			else if (line.find("}") != std::string::npos) {
+                startProduction = false;
+                endProduction = true;
+			}
+			
+			else if (startProduction && !endProduction) {
+				vecData = line.c_str();
+				cout << vecData << "\n";
+				grammarRule->addProduction(hashKey, vecData);
+				
+			}
+			
 		}
 
 		file.close();
+		
+		
 		
 	
 		
