0c68b8e0be13af294158899653209cc72a856064
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2023-04-24 14:53:52 -0400

    Update RandomSentenceGenerator.cpp

--- Lab_5/RandomSentenceGenerator.cpp
+++ Lab_5/RandomSentenceGenerator.cpp
@@ -12,6 +12,7 @@
 #include <iostream>
 #include <sstream>
 #include <fstream>
+#include <algorithm>
 
 using namespace std;
 
@@ -19,54 +20,60 @@ using namespace std;
 		;
 	}	
 	RandomSentenceGenerator::RandomSentenceGenerator(const string & fileName) {
-		
+		// Create a new grammar object.
 		this->grammarRule = new Grammar();
-		
-		ifstream file(fileName);
-		if (!file.is_open()) {
-			cerr << "File not available" << fileName << std::endl;
-		}
-		
-		string hashKey = "";
-		string vecData = "";
-		string line;
-		
-		bool startProduction = false;
-        bool endProduction = true;
-		
-		while (getline(file, line)) {
-			// Parse line here
-			cout << line << endl;
-			if (line.find("{") != std::string::npos) {
-				startProduction = true;
-                endProduction = false;
-                getline(file, line);
-                hashKey = line.c_str();
-				cout << hashKey << "\n";
-			}
-			
-			else if (line.find("}") != std::string::npos) {
-                startProduction = false;
-                endProduction = true;
-			}
-			
-			else if (startProduction && !endProduction) {
-				vecData = line.c_str();
-				cout << vecData << "\n";
-				grammarRule->addProduction(hashKey, vecData);
-				
-			}
-			
-		}
 
-		file.close();
-		
-		
-		
-	
-		
-		
+		// Open input file and create a string to store each line of the file.
+		std::ifstream inputFile;
+		std::string line;
+
+		// Open the input file and check if it is open.
+		inputFile.clear();
+		inputFile.open(fileName.c_str());
+		if (inputFile.is_open())
+		{
+			std::string nonTerminal;
+			bool startOfProduction = false;
+			bool endOfProduction = true;
+
+			// Loop through each line of the file.
+			while (getline(inputFile, line))
+			{
+				// Check if the line contains the start of a new production.
+				if (line.find("{") != std::string::npos)
+				{
+					startOfProduction = true;
+					endOfProduction = false;
+					getline(inputFile, line);
+					nonTerminal = line.c_str();
+					cout << nonTerminal << "\n";
+				}
+				// Check if the line contains the end of a production.
+				else if (line.find("}") != std::string::npos)
+				{
+					startOfProduction = false;
+					endOfProduction = true;
+				}
+				// If the line is part of a production.
+				else if (startOfProduction && !endOfProduction)
+				{
+					line = line.c_str();
+
+					// Remove whitespace from the non-terminal.
+					nonTerminal.erase(remove_if(nonTerminal.begin(), nonTerminal.end(), ::isspace), nonTerminal.end());
+
+					// Add the production to the grammar object.
+					grammarRule->addProduction(nonTerminal, line.substr(0, line.find(";")));
+					cout << line << "\n";
+				}
+			}
+
+			// Close the input file.
+			inputFile.close();
+		}		
 	}
+
+	
 	
 	string RandomSentenceGenerator::randomSentence() {
 		return "Up";
